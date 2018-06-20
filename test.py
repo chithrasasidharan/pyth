722c9fac6125b550f3eff0602d2b5a53958b5eeb
chithrasasidharan <chithratheertham@gmail.com>
2018-06-20 20:13:51 +0530

    2048

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
-import MySQLdb
-
-conn = pymssql.connect(server=server, user=user,password=password,
-	database=db)
-cursor = conn.cursor()
-
-conn.close()
+a = [[0 for x in range(7)] for y in range(7)]
+a[0] = [1,0,1,2,3,0,0]
+a[1] = [3 ,0, 2, 0 ,0, 3, 0]
+n = 7
+i = n-1
+j=n-1
+for k in range(2):
+	i=n-1
+	j=n-1
+	while(i>=0):
+		while(a[k][i]==0):
+			i=i-1
+		a[k][j] = a[k][i]
+		a[k][i] = 0
+		i=i-1
+		j=j-1
+for i in range(2):
+	print(a[i])
