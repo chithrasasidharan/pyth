7ce4c36c48a8bc8fa414439261bfc29e3119588d
chithrasasidharan <chithratheertham@gmail.com>
2018-07-24 20:06:10 +0530

    new1

--- Bloodbank/php_files/retrieve.php
+++ Bloodbank/php_files/retrieve.php
@@ -7,18 +7,12 @@ if ($conn->connect_error) {
 }
 $sql = "SELECT * FROM user WHERE uid='{$_GET['uid']}';";
  if ($res =  mysqli_query($conn,$sql)) {
-    if ($res->num_rows > 0) {
-        while ($row = $res->fetch_array()) 
-        {
-            
-          $ans = array( $row['uid'],$row['Name'],$row['Age'],
-          	$row['Gender'], $row['BloodGroup'],$row['District'],
-          	  $row['State'], $row['PhoneNumber'], $row['Donor'] ) ;
-          $json = json_encode($ans);
-          echo $json;
-       
-        }
+    while ($res->num_rows > 0) {
+            $row = $res->fetch_array()
+            echo json_encode($row)   ;
+           
         $res->free();
+        }
     }
     else {
         echo "No matching records are found.";
