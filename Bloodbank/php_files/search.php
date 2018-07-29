9fe0df7c573bab95869f5a5ffcc99f958d405292
chithrasasidharan <chithratheertham@gmail.com>
2018-07-29 18:44:44 +0530

    f1

--- Bloodbank/php_files/search.php
+++ Bloodbank/php_files/search.php
@@ -1,30 +1,27 @@
-<!-- <?php
-	// include 'class.php';
-	// $b = new Blood();
-	// $dbObj->connect();
-	// $b->search($dbObj);
-	// $dbObj->close();
-?> -->
 <?php
-include 'db.php';
-$dbObj= new Database();
-$dbObj->connect();
-
-$sql ="SELECT uid,Name FROM user WHERE
- BloodGroup='{$_GET['BloodGroup']}';";
-
-$res = $dbObj->executeQuery($sql);
-$rows = array();
-echo $res->num_rows;
-if ($res->num_rows > 0) {
-    while($row = mysqli_fetch_row($res)){
-       $rows[] =  $row;
-    }
-    echo json_encode($rows);
-}
-else {
-    $a = array('errorCode' =>"2",'errorMsg'=>"No matching recd");
-    echo (json_encode($a));
-}
-$dbObj->close();
+	include 'class.php';
+	$b = new Blood();
+	$dbObj->connect();
+	$b->search($dbObj);
+	$dbObj->close();
 ?> 
+<!-- <?php
+// include 'db.php';
+// $dbObj= new Database();
+// $dbObj->connect();
+
+// $sql ="SELECT uid,Name FROM `user` WHERE BloodGroup='{$_GET['BloodGroup']}';";
+// $encsql = rawurlencode($sql);
+// $res = $dbObj->executeQuery($sql);
+// if ($res->num_rows > 0) {
+//     while($row = mysqli_fetch_assoc($res)){
+//     	echo json_encode($row);
+//     }
+    
+// }
+// else {
+//     $a = array('errorCode' =>"2",'errorMsg'=>"No matching recd".$sql);
+//     echo (json_encode($a));
+// }
+// $dbObj->close();
+?>  -->
